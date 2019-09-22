7d5dfbdf0f64a8bf1256aea9a0701eb72cb1cdb3
codertimbro <32404296+codertimbro@users.noreply.github.com>
2019-09-22 18:47:00 -0400

    Confirm if a user is listed in a group

--- findGroups.ps1
+++ findGroups.ps1
@@ -4,9 +4,9 @@ function find-groups{ #Sees if a user in a certain group that is listed in a fil
 "$(Get-Date -format F) Start logging" #Timestamp the task
 param ([string]$user) #The end user inputs the user
 echo $null | clip.exe #Clear out Clipboard
-foreach($line in [System.IO.File]::ReadLines ("C:/groups.txt"){ #This goes out and reads the file line by line and continues until there is no lines yet
- If ($line -contains $user) { #This functions runs to see if the user in the group line by line 
-        Write-Host "$user in group" #This displays an output when the statement is true
+foreach($group in [System.IO.File]::ReadLines ("C:/groups.txt"){ #This goes out and reads the file line by line and continues until there is no lines yet
+ If ($group -contains $user) { #This functions runs to see if the user in the group line by line 
+        Write-Host "$user in the $group group" #This displays an output when the statement is true
         Set-Clipboard #This copies the Wrte-host to clipboard
     } Else {
         Write-Host "$user not in group" # This displays an output
