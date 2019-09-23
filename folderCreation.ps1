753e1a0bb64a60d9bb88c071e1a08604bc22b135
codertimbro <32404296+codertimbro@users.noreply.github.com>
2019-09-22 22:24:43 -0400

    Folder Creation

--- folderCreation.ps1
+++ folderCreation.ps1
@@ -1,3 +1,5 @@
+Write-Host "Folder Creation"
+
 function new-folder {    # Creates a new folder on a network path or at C:/
 "$(Get-Date -format F) Start logging" #Timestamp the task
 param ([string]$path, [string]$new) #parameters the end user will need to input, $path is the network path where the folder is being created at, $new is folder name that is being created
