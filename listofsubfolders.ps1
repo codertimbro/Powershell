c77e40c3089fdbb369e56ec4da96cb89d28f7488
codertimbro <32404296+codertimbro@users.noreply.github.com>
2019-10-18 17:13:17 -0400

    List all the subfolders under a folder

--- listofsubfolders.ps1
+++ listofsubfolders.ps1
@@ -2,3 +2,4 @@ function get-listofsubfolders{
 $path =Read-host "Folder"
 Get-Childitem -Path $path -Directory -Recurse -Force -ErrorAction SilentlyContinue|Select-Object -Property Fullname | Export-csv <file path name> -NoType
 }
+#This scripts get all subfolders list under a path including hidden and system folders.
