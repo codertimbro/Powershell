56b4715bd2d0d36ee21c846493099e81de25a45c
codertimbro <32404296+codertimbro@users.noreply.github.com>
2019-10-18 17:11:32 -0400

    Delete subfolders.ps1

--- subfolders.ps1
+++
@@ -1,8 +0,0 @@
-Write-host "List of subfolders"
-
-function get-subfolders {
-param(Mandatory= $true, [string[]] $path)
-get-childItem -path $path -Directory -Recurse -Force -ErrorAction SilentlyContinue    #This list all the folders diwn the tree and irnotres the error messages 
-Select-Object FullName   #List the full path 
-Export-CSV "<folder path to save list of folders>"
-}
