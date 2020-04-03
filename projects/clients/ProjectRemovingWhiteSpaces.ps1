111cd207b6bfcec3ef3e5470df69cebca45bad78
codertimbro <32404296+codertimbro@users.noreply.github.com>
2020-04-03 15:41:32 -0400

    Delete ProjectRemovingWhiteSpaces.ps1

--- projects/clients/ProjectRemovingWhiteSpaces.ps1
+++
@@ -1,6 +0,0 @@
-function format-spacesinafile{
-Import-Csv -Path <orginal file with spaces in it>.csv 
-ConvertTo-Csv -NoTypeInformation #Converts the data to be display when it is export to a file. Without this, you will get @{<string data>}
-ForEach-Object {$_ -replace "\s+" , "" } #replaces whitespaces
-Out-file <new file name with the newly format data>.csv -Encoding ascii -append
-}
